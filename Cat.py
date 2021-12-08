ec1c2deb760a1630ea98f2695885eed7ca7f121f
AndreyDemdi <95755476+AndreyDemdi@users.noreply.github.com>
2021-12-08 16:16:12 +0300

    Create Cat.py

---
+++ Cat.py
@@ -0,0 +1,9 @@
+#the class cat
+#Author AndreyDemdi
+#version 8.12.2021
+class Cat:
+    def __init__(self,name,age,breed):
+        self.name = name 
+        self.age = age
+        self.breed = breed
+    Cat=print("name Stepa","age=2","breed=Scottish");
